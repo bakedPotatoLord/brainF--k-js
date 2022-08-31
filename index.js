34c7ca3be82114c83f7517cabc5da91e2dcc07e8
BakedPotatoLord <jeffy9745@gmail.com>
2022-08-31 15:25:40 -0600

    started interpreting

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
-import fs from 'fs/promises'
+import fs from 'fs'
 
 
 
-await fs.readFile(process.argv[2])
+fs.readFile(process.argv[2],'utf-8',(err,data)=>{
+    if(err)console.log(err)
+    console.log(data)
 
-let arr = Array.from(new Array(40000))
+    let arr = Array.from(new Array(40000))
+    let pointer = 0
+
+    data = data.split('')
+    for(i of data){
+        if(i == '+'){
+            arr[pointer]++
+        }else if(i == '-'){
+            arr[pointer]--
+        }else if(i == '>'){
+            pointer++
+        }else if(i == '<'){
+            pointer--
+        }else if(i == '.'){
+            arr[pointer]++
+        }else if(i == ','){
+            arr[pointer]++
+        }else if(i == '['){
+            arr[pointer]++
+        }else if(i == ']'){
+            arr[pointer]++
+        }
+    }
+})
+
