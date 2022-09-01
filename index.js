b8db8e69aaaee51d2af3ccb9052e906102a331b0
= <918780@dpsk12.net>
2022-08-31 20:20:42 -0600

    almost finished it

--- index.js
+++ index.js
@@ -1,33 +1,53 @@
-import fs from 'fs'
+import fs from 'fs/promises'
 
-
-
-fs.readFile(process.argv[2],'utf-8',(err,data)=>{
-    if(err)console.log(err)
-    console.log(data)
-
-    let arr = Array.from(new Array(40000))
-    let pointer = 0
-
-    data = data.split('')
-    for(i of data){
-        if(i == '+'){
-            arr[pointer]++
-        }else if(i == '-'){
-            arr[pointer]--
-        }else if(i == '>'){
-            pointer++
-        }else if(i == '<'){
-            pointer--
-        }else if(i == '.'){
-            arr[pointer]++
-        }else if(i == ','){
-            arr[pointer]++
-        }else if(i == '['){
-            arr[pointer]++
-        }else if(i == ']'){
-            arr[pointer]++
+function execute(char){
+    if(char == '+'){
+        arr[pointer]++
+    }else if(char == '-'){
+        arr[pointer]--
+    }else if(char == '>'){
+        pointer++
+    }else if(char == '<'){
+        pointer--
+    }else if(char == '.'){
+       outArr.push(arr[pointer])  
+    }else if(char == ','){
+        arr[pointer] = inArr[inIndex]
+        inIndex++
+    }else if(char == '['){
+        loopArr.push(pointer)
+    }else if(char == ']'){
+        if(arr[pointer] != 0){
+            pointer = loopArr[loopArr.length-1]
+            loopArr.pop()
+        }else{
+            
         }
     }
-})
+}
+
+function getFileExtension(filename) {
+    return  filename.split('.').pop();
+}
+
+var arr = Array.from(new Uint8ClampedArray(40000),x=>0)
+var outArr = []
+let pointer = 0
+let loopArr = []
+
+try{
+    await fs.open('input.txt')
+}catch(err){
+    throw new Error('cannot find input.txt')
+}
+var inIndex = 0
+var inArr = await fs.readFile(process.argv[2],'utf-8')
+var data = await fs.readFile(process.argv[2],'utf-8')
+    if(!getFileExtension(process.argv[2])== 'bf' || !getFileExtension(process.argv[2])== 'b') throw new Error('filename must end in .b or .bf')
+
+    data = data.split('')
+    for(let i of data){
+        execute(i)
+    }
+    console.log(arr)
 
