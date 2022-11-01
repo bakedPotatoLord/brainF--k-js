0f4eb0db7339c451b9202a680afeb743be657c61
= <918780@dpsk12.net>
2022-10-31 19:27:54 -0600

    changed colors

--- bfi.ts
+++ bfi.ts
@@ -2,19 +2,20 @@
 import { interpret } from "./main.js";
 import * as fs from "node:fs/promises"
 
-console.log("interpretation starting")
+console.log('\x1b[36m%s\x1b[0m',"BFInterpreter starting")
 
 try{
   await fs.open('input.txt')
 }catch(err){
   await fs.writeFile('input.txt','','ascii')
-  console.log("automatically generated input.txt")
+  console.log("\x1b[0m","automatically generated input.txt")
 }
-let inArr = Array.from(String(await fs.readFile('input.txt','ascii')).split(''),x=>x.charCodeAt(0))
+let inArr = Array.from((await fs.readFile('input.txt','ascii')).split(''),x=>x.charCodeAt(0))
 try{
   var data = (await fs.readFile(process.argv[2],'utf-8')).split('')
 }catch(err){
-  throw new Error("path not found")
+  console.log("\x1b[31m","ERROR: must pass in program to execute\nex: bfi <path-to-program>")
+  process.exit()
 }
 
 interpret(data,inArr)
