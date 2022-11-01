4bddfd1e9768cfda628ed1700e5f99310206ea2e
= <918780@dpsk12.net>
2022-10-31 19:39:41 -0600

    fixed colors and crap

--- main.ts
+++ main.ts
@@ -15,15 +15,15 @@ function execute(char:string,inArr:number[]){
     }else if(char == '<'){
         pointer--
     }else if(char == '.'){
-       outArr.push(arr[pointer])  
+       outArr.push(arr[pointer])
     }else if(char == ','){
         arr[pointer] = inArr[inIndex]
-				if(inArr[inIndex] == undefined) arr[pointer] = 0
-				inIndex++
+        if(inArr[inIndex] == undefined) arr[pointer] = 0
+        inIndex++
     }else if(char == '['){
         loopArr.push(instructionIndex)
     }else if(char == ']'){
-			if(loopArr.length == 0) throw new Error("unmatched ]")
+			if(loopArr.length == 0){ console.log("\x1b[31m","ERROR: unmatched ]"); process.exit()}
         if(arr[pointer] != 0){
             instructionIndex = loopArr[loopArr.length-1]
         }else{
@@ -37,7 +37,7 @@ export async function interpret(program:string[],inputArr:number[]){
             execute(program[instructionIndex],inputArr)     
             instructionIndex++ 
     }
-    console.log(arr)
+    console.log("\x1b[0m",arr)
     console.log(`pointer: ${pointer}, value: ${arr[pointer]}`)
     console.log(`\nOutput: ${new TextDecoder().decode(new Uint8ClampedArray(outArr))}`)
 }
