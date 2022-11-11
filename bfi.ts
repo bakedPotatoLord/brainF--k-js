9b8b95146cf6de7da06f26fe228c4cb11cc37dd4
bakedpotatolord <918780@dpsk12.net>
2022-11-11 13:21:36 -0700

    colored with chalk

--- bfi.ts
+++ bfi.ts
@@ -1,21 +1,22 @@
 #!/usr/bin/env node
-import { interpret } from "./main";
+import { interpret } from "./main.js";
 import * as fs from "node:fs/promises"
+import chalk from "chalk"
 
 
-console.log('\x1b[36m%s\x1b[0m',"BFInterpreter starting")
+console.log(chalk.blueBright("BFInterpreter starting"))
 
 try{
   await fs.open('input.txt')
 }catch(err){
   await fs.writeFile('input.txt','','ascii')
-  console.log("\x1b[0m"+"Automatically generated input.txt")
+  console.log(chalk.white("Automatically generated input.txt"))
 }
 let inArr:number[] = Array.from((await fs.readFile('input.txt','ascii')).split(''),x=>x.charCodeAt(0))
 try{
   var data = (await fs.readFile(process.argv[2],'utf-8')).split('')
 }catch(err){
-  console.log("\x1b[31m"+"ERROR: must pass in program to execute\nex: bfi <path-to-program>")
+  console.log(chalk.red("ERROR: must pass in program to execute\nex: bfi <path-to-program>"))
   process.exit()
 }
 
